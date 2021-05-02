e492bce11b7c0daa7ceb0194eb955910267cd150
gabrielukke <gabriellukk@outlook.com>
2021-05-02 02:16:47 -0300

    Add to localstorage functions(Not Finished)

--- script.js
+++ script.js
@@ -19,25 +19,86 @@ const getFruits = async () => await fetch(URL, options).then(data => data.json()
 
 // const getFruits = async () => await (await fetch(URL, options)).json();
 
-const Fruit = (fruit) => (`
+// Componente Fruit
+/* const Fruit = (fruit) => (`
     <li>
       <h2>${fruit.name}</h2>
       <h4>${fruit.calories}</h4>
       <div>
         <img src="${fruit.photo}" alt="fruit" width="200"/>
       </div>
+      <button type="button" id="favorite">Adicionar a Favoritos</button>
     </li>
   `
-)
+) */
 
+let localStorageFruits = [];
+// let localStorageFavoriteFruits = [];
+
+const addFruitsToLocalStorage = (fruits) => {
+  localStorage.setItem('fruits', JSON.stringify(fruits));
+  localStorageFruits = [...fruits];
+};
+
+const addFruitToFavorites = (index) => {
+  // Pega a lista de favoritos do LocalStorage
+  const localStorageFavoriteFruits = JSON.parse(localStorage.getItem('favoriteFruits'));
+  
+  // Verifica se a lista existe e se há alguma fruta nela
+  if (!!localStorageFavoriteFruits && localStorageFavoriteFruits.length > 0) {
+    const isFavorited = localStorageFavoriteFruits.some((fruit) => fruit === localStorageFruits[index]);
+
+    console.log(isFavorited);
+    
+    if (isFavorited) return false;
+
+    const toLocalStorageFF = [...localStorageFavoriteFruits, localStorageFruits[index]];
+    console.log(localStorageFavoriteFruits);
+    localStorage.setItem('favoriteFruits', JSON.stringify(toLocalStorageFF));
+    return true
+  }
+
+  // Cria um array com apenas uma fruta e adiciona ao Local Storage
+  const fruit = localStorageFruits[index];
+  const newFruitList = [fruit];
+  localStorage.setItem('favoriteFruits', JSON.stringify(newFruitList));
+  return true
+};
+
+// Componente Fruit
+const Fruit = (fruit, index) => {
+  const fruitElement = document.createElement('li')
+  fruitElement.innerHTML = `
+    <h2>${fruit.name}</h2>
+    <h4>${fruit.calories}</h4>
+    <div>
+      <img src="${fruit.photo}" alt="fruit" width="200"/>
+    </div>
+  `
+  const button = document.createElement('button');
+  button.innerText = 'Add to Favorites';
+  button.addEventListener('click', () => {
+    addFruitToFavorites(index);
+  })
+  fruitElement.appendChild(button);
+  return fruitElement;
+};
+
+// Renderiza as frutas na tela
 const renderFruits = (fruits) => {
   const olSelector = document.querySelector('#fruits-list');
-  fruits.forEach((fruit) => {
-    olSelector.innerHTML += Fruit(fruit);
+  fruits.forEach((fruit, index) => {
+    olSelector.appendChild(Fruit(fruit, index));
   });
+};
+
+const verifyLocalStorageFruits = async () => {
+  const localStorageFruits = JSON.parse(localStorage.getItem('fruits'));
+  return !!localStorageFruits ? localStorageFruits : await getFruits();
 }
 
 window.onload = async () => {
-  const fruits = await getFruits();
+  const fruits = await verifyLocalStorageFruits();
+  addFruitsToLocalStorage(fruits);
   renderFruits(fruits);
-}
+};
